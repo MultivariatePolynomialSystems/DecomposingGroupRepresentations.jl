38753d52681151b625c739467ba8b60f647b9a7f
Documenter.jl <documenter@juliadocs.github.io>
2025-02-01 08:49:54 +0000

    build based on 98a1b1c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DecomposingRepresentations","category":"page"},{"location":"#DecomposingRepresentations","page":"Home","title":"DecomposingRepresentations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DecomposingRepresentations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DecomposingRepresentations]","category":"page"}]
+}
